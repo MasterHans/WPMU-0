f727e7f9243b91efee0f29ce55b5826ba348dacc
masterhans <suvorov.ag.75@gmail.com>
2017-08-24 23:00:19 +0300

    Add some templates

---
+++ wp-content/themes/wpmudev-intermediate-dev-part1/page.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<div class="container">
+	<div class="content">
+	<?php if ( have_posts()) :?>
+		<?php while (have_posts()) : the_post();?> 
+
+		<article>
+			
+			<?php if (is_front_page() ) {
+				return;	
+			}
+			else { ?>
+				<h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h1>
+			<?php } ?>
+			
+			<div class="entry-content">
+				<?php the_content('<p class="left"></p>');?>
+			</div><!--.entry-content-->		
+		
+		</article>	
+
+		<?php endwhile; endif; ?>
+
+	</div><!--.content-->
+	
+<?php get_sidebar(); ?>
+
+</div><!--.container-->
+
+<?php get_footer(); ?>
--- wp-content/themes/wpmudev-intermediate-dev-part1/single.php
+++ wp-content/themes/wpmudev-intermediate-dev-part1/single.php
@@ -7,7 +7,7 @@
 
 		<article>
 
-			<h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h2>
+			<h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title();?></a></h1>
 
 			<div class="entry-content">
 				<?php the_content('<p class="left"></p>');?>
