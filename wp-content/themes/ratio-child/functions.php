4abbbe29fca124f4739a87f22a3a992a71290b71
MasterHans <suvorov.ag.75@gmail.com>
2017-09-11 10:35:54 +0300

    New approach in enquening the parent script

--- wp-content/themes/ratio-child/functions.php
+++ wp-content/themes/ratio-child/functions.php
@@ -3,10 +3,12 @@
 /***  Enqueue child theme stylesheet ***/
 
 function ratio_edge_child_enqueue_style() {
-	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-	wp_enqueue_style( 'childstyle' );
+	// enqueue parent styles
+	wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
+//	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+//	wp_enqueue_style( 'childstyle' );
 }
-add_action( 'wp_enqueue_scripts', 'ratio_edge_child_enqueue_style', 11);
+add_action( 'wp_enqueue_scripts', 'ratio_edge_child_enqueue_style');
 
 /*include the function for related posts displaying*/
 require_once get_stylesheet_directory() . '/includes/sag-edgtf-related-posts.php';
