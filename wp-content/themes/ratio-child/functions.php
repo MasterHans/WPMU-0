183d2b6358ed0a9f47a7bee18f8bb4d840da178f
MasterHans <suvorov.ag.75@gmail.com>
2017-09-11 09:54:09 +0300

    Add copy Child-theme

---
+++ wp-content/themes/ratio-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/***  Enqueue child theme stylesheet ***/
+
+function ratio_edge_child_enqueue_style() {
+	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+	wp_enqueue_style( 'childstyle' );
+}
+add_action( 'wp_enqueue_scripts', 'ratio_edge_child_enqueue_style', 11);
+
+/*include the function for related posts displaying*/
+require_once get_stylesheet_directory() . '/includes/sag-edgtf-related-posts.php';
