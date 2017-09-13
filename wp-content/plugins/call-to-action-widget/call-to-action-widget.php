a42e62b96221991e263590fda2551a49c76c1d80
MasterHans <suvorov.ag.75@gmail.com>
2017-09-13 18:07:46 +0300

    Add plugin widget

--- wp-content/plugins/call-to-action-widget/call-to-action-widget.php
+++ wp-content/plugins/call-to-action-widget/call-to-action-widget.php
@@ -6,8 +6,6 @@
  * Version:       1.0
  * Author:        Rachel McCollin
  * Author URI:    http://rachelmccollin.co.uk
- *
- *
  */
 
 /*********************************************************************************
@@ -15,8 +13,67 @@ Enqueue stylesheet
  *********************************************************************************/
 function wpmu_widget_enqueue_styles() {
 
-    wp_register_style( 'cta_css', plugins_url( 'css/style.css', __FILE__ ) );
+    wp_register_style( 'cta_css', plugins_url( 'public/css/style.css', __FILE__ ) );
     wp_enqueue_style( 'cta_css' );
 
 }
 add_action( 'wp_enqueue_scripts', 'wpmu_widget_enqueue_styles' );
+
+class Wpmu_Cta_Widget extends WP_Widget {
+    function __construct() {
+        $widget_options = array(
+            'classname' => 'wpmu_cta_widget',
+            'description' => 'Add a Call to Action box encouraging people to get in touch.'
+        );
+        parent::__construct( 'wpmu_cta_widget', 'Call to Action', $widget_options );
+    }
+    function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+        $tel = ! empty( $instance['tel'] ) ? $instance['tel'] : 'Telephone number';
+        $email = ! empty( $instance['email'] ) ? $instance['email'] : 'Email address';
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+            <input class="widefat" type ="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id( 'tel' ); ?>">Your telephone number:</label>
+            <input class="widefat" rows="10" type="text" id="<?php echo $this->get_field_id( 'tel' ); ?>" name="<?php echo $this->get_field_name( 'tel' ); ?>" value="<?php echo esc_attr( $tel ); ?>" />
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id( 'email' ); ?>">Your email address:</label>
+            <input class="widefat" rows="10" type="text" id="<?php echo $this->get_field_id( 'email' ); ?>" name="<?php echo $this->get_field_name( 'email' ); ?>" value="<?php echo esc_attr( $email ); ?>" />
+        </p>
+
+    <?php }
+
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+        $instance[ 'tel' ] = strip_tags( $new_instance[ 'tel' ] );
+        $instance[ 'email' ] = strip_tags( $new_instance[ 'email' ] );
+        return $instance;
+    }
+
+    function widget( $args, $instance ) {
+        echo $args ['before_widget'];
+        $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+        $tel = $instance['tel'];
+        $email = $instance['email'];
+        ?>
+        <div class="cta">
+            <?php if ( ! empty( $title ) ) {
+                echo $args['before_title'] . $title . $args['after_title'];
+		}; ?>
+            <?php echo 'Call us on ' . $tel . ' or email <a href="' . $email . '">' . $email . '</a>'; ?>
+        </div>
+
+        <?php echo $args['after_widget'];
+    }
+}
+function wpmu_register_widget() {
+    register_widget( 'Wpmu_Cta_Widget' );
+}
+add_action( 'widgets_init', 'wpmu_register_widget' );
